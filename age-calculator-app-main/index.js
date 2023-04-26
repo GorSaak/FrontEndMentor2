6909189a22540a782bd0fe9b25241af423f6a946
Gor Sahakyan <saakyangor@gmail.com>
2023-04-26 11:38:53 +0400

    js

--- age-calculator-app-main/index.js
+++ age-calculator-app-main/index.js
@@ -1,8 +1,30 @@
 const date = new Date();
 
+let year = date.getFullYear();
+let month = date.getMonth()+1;
+let day = date.getDate();
+
+const a = document.getElementById("day-input")
+console.log(a)
+    a.addEventListener("change", () => {
+    if (Number.isInteger(+a.value) && 1<=a.value && a.value <= 31) {
+       alert("true")
+    } else {
+      // let b = document.getElementById("day-input").innerHTML
+        console.log(+b.value-500)
+        alert("false")
+    }
+})
 
 function getTheAge() {
-    let a = document.getElementById("day-input").value;
+    let inpDay = document.getElementById("day-input").value;
+    let inpMonth = document.getElementById("month-input").value;
+    let inpYear = document.getElementById("year-input").value;
 
-    alert(date.getFullYear())
+    console.log(inpDay, inpMonth, inpYear)
+    // console.log(year - inpYear)
+    
+    document.getElementById("year-change").innerHTML = year - inpYear
+    document.getElementById("month-change").innerHTML = month - inpMonth
+    document.getElementById("day-change").innerHTML = day - inpDay
 }
